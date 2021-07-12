803c99d0dd3d97703b0c649f12b04991872b98de
Artin.Esrailian <artine@energizeglobal.com>
2021-07-12 10:23:01 +0400

    fixed the erros on auto merger approval

--- number_in_words.py
+++ number_in_words.py
@@ -90,7 +90,7 @@ def main():
     spell(number)
 main()
 
-def sumTwoTerms(term1, term2):
+def sum_two_terms(term1, term2):
    return term1 + term2
 
-print("2 + 2 = {0}".format(sumTwoTerms(2, 2)))
+print("2 + 2 = {0}".format(sum_two_terms(2, 2)))
